b3d2d9882c86623d9dd88bb22ae4a5259e7d1edd
MasakiFukunishi <fmsknn@gmail.com>
2023-01-12 09:11:25 -0800

    Add Palindrome Number

--- easy/9_palindrome-number.js
+++ easy/9_palindrome-number.js
@@ -1,1 +1,37 @@
 // Follow up: Could you solve it without converting the integer to a string?
+
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+// const isPalindrome = function (x) {
+//   const stringX = String(x);
+//   for (let i = 0; i < Math.floor(stringX.length - 1) / 2; i++) {
+//     const front = stringX[i];
+//     const back = stringX[stringX.length - 1 - i];
+//     if (front !== back) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// Time complexity: O(log10(n))
+// Space complexity: O(1)
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = function (x) {
+  if (x < 0) return false;
+  let number = x;
+  let reversed = 0;
+  while (number > 0) {
+    reversed = reversed * 10 + (number % 10);
+    number = parseInt(number / 10);
+  }
+  return x === reversed;
+};
+console.log(isPalindrome(121));
