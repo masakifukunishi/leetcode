df22504c84f07119cb33772f1cfe4e96a34d4592
MasakiFukunishi <fmsknn@gmail.com>
2023-01-11 16:10:09 -0800

    Fix Two Sum

--- easy/1_tow-sum.js
+++ easy/1_tow-sum.js
@@ -5,15 +5,15 @@
  * @param {number} target
  * @return {number[]}
  */
-function twoSum(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
-  }
-}
+// const twoSum = (nums, target) => {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// };
 
 // Time complexity: O(n)
 // Space complexity: O(n)
@@ -22,15 +22,15 @@ function twoSum(nums, target) {
  * @param {number} target
  * @return {number[]}
  */
-function twoSum(nums, target) {
-  let hashmap = {};
+const twoSum = (nums, target) => {
+  const hash = {};
   for (let i = 0; i < nums.length; i++) {
-    let complement = target - nums[i];
-    if (hashmap[complement] !== undefined) {
-      return [i, hashmap[complement]];
+    const complement = target - nums[i];
+    if (hash[complement] !== undefined) {
+      return [i, hash[complement]];
     }
-    hashmap[nums[i]] = i;
+    hash[nums[i]] = i;
   }
-}
+};
 
-// console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 7, 11, 15], 9));
