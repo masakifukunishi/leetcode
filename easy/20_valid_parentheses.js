c62a1a3813361b042f752a30cc638ddcbdbf4ceb
MasakiFukunishi <fmsknn@gmail.com>
2023-01-12 16:13:48 -0800

    Add valid parentheses

---
+++ easy/20_valid_parentheses.js
@@ -0,0 +1,23 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = function (s) {
+  const map = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (stack.length > 0 && map[stack[stack.length - 1]] === s[i]) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+  return stack.length === 0;
+};
+console.log(isValid("(()())"));
