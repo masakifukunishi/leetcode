91034fa7fe403f0d59e18dff0be91c6afbe49df9
MasakiFukunishi <fmsknn@gmail.com>
2023-01-24 11:33:37 -0800

    Add 141 linked list cycle

---
+++ easy/141-linked-list-cycle.js
@@ -0,0 +1,13 @@
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle1 = (head) => {
+  const list = [];
+  while (head) {
+    if (list.includes(head.val)) return true;
+    list.push(head.val);
+    head = head.next;
+  }
+  return false;
+};
