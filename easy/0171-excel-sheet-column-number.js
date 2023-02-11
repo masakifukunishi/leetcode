41e1e5e2de97dc45764d38397928cd625cbd49ed
MasakiFukunishi <fmsknn@gmail.com>
2023-02-10 17:46:35 -0800

    Add 0171-excel-sheet-column-number

---
+++ easy/0171-excel-sheet-column-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+const titleToNumber = (columnTitle) => {
+  let result = 0;
+  for (let i = columnTitle.length - 1, k = 0; i >= 0; i--, k++) {
+    result += (columnTitle.charCodeAt(i) - 64) * 26 ** k;
+  }
+  return result;
+};
